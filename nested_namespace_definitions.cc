aaf9934e39956ed3ba1c2e3b78e9cf29c1a64421
Dominik Berner <dominikberner@bbv.ch>
2017-06-08 10:40:04 +0200

    SDS

--- nested_namespace_definitions.cc
+++ nested_namespace_definitions.cc
@@ -3,14 +3,8 @@ void doMagic() { /* Magic happens here */
 }
 }
 
-namespace A::B {
-void callMagician() { C::doMagic(); }
-}
-
 int main(int argc, char **argv) {
   A::B::C::doMagic();
-
-  A::B::callMagician();
 
   return 0;
 }
