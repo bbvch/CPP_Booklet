e0438634a895f8b17b1e87aca33d1b9444093422
Dominik Berner <dominik.berner@bbv.ch>
2017-06-08 11:57:33 +0200

    some renaming to fit with the magic theme

--- noexcept.cc
+++ noexcept.cc
@@ -1,5 +1,4 @@
 #include <exception>
-#include <iostream>
 
 namespace {
 class myException : std::exception {
@@ -9,16 +8,12 @@ class myException : std::exception {
 
 class Magician {
 public:
-  int magic() const noexcept { // marked as not throwing
-    std::cout << "Execute me\n";
-    return _magic;
-  }
-  void doMagic() noexcept(false) { // marked as possibly throwing
+  void riskyMagic() noexcept(false) { // marked as possibly throwing
     if (_magic < 500) {
       throw myException();
     }
   }
-  void exceptionFreeMagic() noexcept { /** do some magic */
+  void magic() noexcept { /** do some magic */
   }
 
   void badMagic() noexcept {
@@ -30,10 +25,10 @@ public:
   void unspecifiedMagic() { /** do some magic **/
   }                         // same as noexcept(false)
 
-  void failureFreeMagic() throw() { /** do some magic **/
+  void oldMagic() throw() { /** do some magic **/
   }
 
-  void failingMagic() throw(myException) { throw myException(); }
+  void oldriskyMagic() throw(myException) { throw myException(); }
 
 private:
   int _magic{1000};
@@ -42,16 +37,11 @@ private:
 int main(int argc, char **argv) {
   Magician m;
 
-  // does NOT execute magic!
-  static_assert(noexcept(m.magic()), "magic is noexcept");
-  static_assert(
-      noexcept(m.exceptionFreeMagic()),
-      "this magic is exception free"); // does NOT execute the function
-  static_assert(!noexcept(m.doMagic()), "magic is noexcept"); //
-  static_assert(!noexcept(m.unspecifiedMagic()),
-                "unspecified functions are possibly throwing");
+  static_assert(noexcept(m.magic()), "not throwing");
+  static_assert(!noexcept(m.riskyMagic()), "not throwing");
+  static_assert(!noexcept(m.unspecifiedMagic()), "possibly throwing");
   static_assert(noexcept(m.badMagic()),
                 "bad magic pretends to be not throwing");
-  static_assert(noexcept(m.failureFreeMagic()), "throwing magic may throw");
-  static_assert(!noexcept(m.failingMagic()), "throwing magic may throw");
+  static_assert(noexcept(m.oldMagic()), "not throwing");
+  static_assert(!noexcept(m.oldriskyMagic()), "possibly throwing");
 }
