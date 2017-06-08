f2145e893e998c22eb8eeaca39e831f78afc0203
Dominik Berner <dominik.berner@bbv.ch>
2017-06-08 11:52:11 +0200

    Exdented example with deprecated 'throw()' statements

--- noexcept.cc
+++ noexcept.cc
@@ -9,27 +9,31 @@ class myException : std::exception {
 
 class Magician {
 public:
-  int magic() const noexcept {
+  int magic() const noexcept { // marked as not throwing
     std::cout << "Execute me\n";
     return _magic;
-  }                                // marked as not throwing
+  }
   void doMagic() noexcept(false) { // marked as possibly throwing
     if (_magic < 500) {
       throw myException();
     }
   }
-  void exceptionFreeMagic() noexcept {
-    /** do some magic */
+  void exceptionFreeMagic() noexcept { /** do some magic */
   }
 
   void badMagic() noexcept {
-      // putting a throw-statement inside this function will NOT break compilation
-      // even if it violates the noexcept specifier
-      throw myException();
+    // putting a throw-statement inside this function will NOT break compilation
+    // even if it violates the noexcept specifier
+    throw myException();
   }
 
-  void unspecifiedMagic() { /** do some more magic **/
+  void unspecifiedMagic() { /** do some magic **/
+  }                         // same as noexcept(false)
+
+  void failureFreeMagic() throw() { /** do some magic **/
   }
+
+  void failingMagic() throw(myException) { throw myException(); }
 
 private:
   int _magic{1000};
@@ -48,5 +52,6 @@ int main(int argc, char **argv) {
                 "unspecified functions are possibly throwing");
   static_assert(noexcept(m.badMagic()),
                 "bad magic pretends to be not throwing");
-
+  static_assert(noexcept(m.failureFreeMagic()), "throwing magic may throw");
+  static_assert(!noexcept(m.failingMagic()), "throwing magic may throw");
 }
