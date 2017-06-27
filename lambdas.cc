1cb69bfa52b230adf61cec0cca6c9d4ac4a8f6aa
Dominik Berner <dominik.berner@bbv.ch>
2017-06-27 13:38:04 +0200

    Added example for lambdas

---
+++ lambdas.cc
@@ -0,0 +1,36 @@
+#include <vector>
+#include <algorithm>
+
+int main(int, char**)
+{
+    const auto square = [](int x) { return x*x;};
+    const auto r = square(4);
+
+    int x, y, z;
+
+    const auto no_capture = [] () {}; // keine Zugriffsdeklaration
+    const auto capture_by_value = [x, y] () {}; // Variablen by value
+    const auto capture_by_ref = [&x, &y] () {}; // Variablen by reference
+    const auto mixed_capture = [x, &y, &z] () {}; // gemischter Zugriff
+    const auto capture_all_by_value = [=] () {}; // captures all variables in scope by value (square, r, x,y,z)
+    const auto capture_all_by_ref= [&] () {}; // alle Variablen by reference
+    const auto capture_all_by_value_except = [=, &x] () {}; // alle by value ausser x by ref
+    const auto captire_all_by_ref_except = [&, x] () {}; // Capture all by reference except x
+
+    // using lambda with stransform
+    std::vector<int> a1{1, 2, 3, 4, 5};
+    std::vector<int> a2(a1.size());
+    // Fülle einen Vektor mit den Quadraten eines anderen
+    // Vektors
+    std::transform(a1.begin(), a1.end(), a2.begin(),
+    square );
+
+    std::vector<int> v{1, 2, 3, 4, 5};
+    int sum {0};
+    // Summe der Elemente von v
+    std::for_each(v.begin(), v.end(), [&sum] (int i)
+    {sum+=i;});
+
+    std::transform(a1.begin(), a1.end(), a2.begin(),
+    [](int x) -> int { int y = x % 5; return x * y; } );
+}
