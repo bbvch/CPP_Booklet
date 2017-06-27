de68e875cf29246ca315210fe8cbbd718be5d6c8
Dominik Berner <dominik.berner@bbv.ch>
2017-06-27 12:56:08 +0200

    Added example for inheritance related keywords for classes

---
+++ class_inheritance_keywords.cc
@@ -0,0 +1,38 @@
+struct NonDeriveable final {};
+
+// Error cannot derive from Base because of final
+// struct Derived : NonDeriveable {};
+
+struct BaseWithFinalMembers {
+  virtual void foo(int) final;
+  virtual void bar();
+};
+struct Derived2 : public BaseWithFinalMembers {
+
+  // Error
+  // void foo(int);
+  void bar() override; // OK
+};
+
+class DelegatingCtor {
+  int m_number;
+
+public:
+  DelegatingCtor(int new_number) : m_number(new_number) {}
+  DelegatingCtor() : DelegatingCtor(42) {} // delegated
+};
+
+struct Base {
+  explicit Base(int z) {};
+    Base() = default;
+};
+struct InheritingCtor : public Base {
+  using Base::Base; // alle vererbten Konstruktoren
+  // aufnehmen
+  InheritingCtor(int x, int y) {}; // neue Überladung
+};
+
+int main(int, char **) {
+  InheritingCtor d1(5, 10); // neuen Konstruktor verwenden
+  InheritingCtor d2(42);    // mit using eingeführten Konstruktor
+}
