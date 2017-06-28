6591968bce653374dd4c8829b5b11a4c654aaa52
Dominik Berner <dominik.berner@@bbv.ch>
2017-06-28 15:49:03 +0200

    added comments and clarified example for unified initialisation

--- unified_initialisation.cc
+++ unified_initialisation.cc
@@ -3,6 +3,8 @@
 * of the C++ Booklet (https://goo.gl/VJ4T3A)
 * published by bvv software services AG (c) 2017
 *
+* This example illustrates the unified syntax for initializing stuff in
+*C++11/14/17
 **/
 
 #include <string>
@@ -13,17 +15,23 @@ public:
   Klass(int x, const std::string &s) : _x(x), _s(s) {}
 
 private:
-  int _x{999};
-  int _y = 1000;
+  // initialisation can be specified at the time of declaration
+
+  int _x{999};   // Value assignement through construction
+  int _y = 1000; // values assignment
   std::string _s;
 };
 
 int main(int, char **) {
 
-  // simple initializing
+  // simple initializing usion {}
   Klass k{123, "b"};
   int x{42};
-  std::vector<int> v{1, 2, 3, 4};
+
+#ifdef EXPECT_FAILED_COMPILATION
+  int fail{7.5f}; // fails because of narrowing down from floata to int
+#endif
+  std::vector<int> v{1, 2, 3, 4}; // list initialisation
   std::vector<int> y = {0, 1, 2, 3, 4};
 
   // initializing of aggregates
@@ -33,22 +41,21 @@ int main(int, char **) {
     std::string c;
   };
   Data data{7, 3.14, "Seven Pies!"};
-  // Data data2 {7.0, 3.14, "Seven Pies!"}; // Error narrowing from double to
-  // int
+#ifdef EXPECT_FAILED_COMPILATION
+  // Error narrowing from double to int
+  Data data2{7.0, 3.14, "Seven Pies!"};
+#endif
 
   struct MyType {
-    // der Konstruktor macht aus der Klasse
-    // ein non-aggregate
+    // the constructor makes a non-aggregate out of this struct
     MyType(const std::string &msg, int x) : m_a(x), m_c(msg) {}
     int m_a;
     double m_b;
     std::string m_c;
   };
-  MyType myType{"Hello World!", 42};
+  MyType myType{"Fortytwo!", 42};
 
-  return 0;
-
-  // Example for a container-type
+  // Example for a container-type with explicit list initialisation
   struct Bar {
     explicit Bar(std::initializer_list<std::string> in) {
       for (auto &s : in)
