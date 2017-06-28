6673ca02c797d8df7ecdd8a365d2a34f073a2bb7
Dominik Berner <dominik.berner@@bbv.ch>
2017-06-28 15:25:52 +0200

    Reformated comments for standard attributes example

--- standard_attributes.cc
+++ standard_attributes.cc
@@ -3,6 +3,7 @@
 * of the C++ Booklet (https://goo.gl/VJ4T3A)
 * published by bvv software services AG (c) 2017
 *
+* This example illustrates the usage of standard-attributes
 **/
 
 #include <atomic>
@@ -16,34 +17,15 @@ void foo() {}
 void bar() {}
 }
 
+// indicates that this function never returns
 [[noreturn]] void always_throw() { throw 123; };
 
-void opaque_func(int *p){/* do something with p */};
+// generates a compiler warning if called
+[[deprecated("black magic is no longer used")]] void black_magic(){};
 
-[[carries_dependency]] void transparent_func(int *p){
-    /* do something with p */
-}
+// generates a compiler warning if called
+[[deprecated]] void ancient_magic() {}
 
-    [[deprecated("black magic is no longer used")]] void black_magic(){}[
-        [deprecated]] void ancient_magic() {}
-
-void white_magic();
-
-void illustrate_carries_dependency() {
-  std::atomic<int *> p;
-  int *atomic = p.load(std::memory_order_consume);
-
-  if (atomic)
-    std::cout << *atomic << std::endl; // transparent for the the compiler
-
-  if (atomic)
-    opaque_func(atomic); // if from another compile unit and not inline the
-                         // compiler might construct a memory fence here
-
-  if (atomic)
-    transparent_func(atomic); // marked as to work in the same memory-dependency
-                              // tree, compiler can omit the memory fence
-}
 
 void fall_through(int i) {
   switch (i) {
@@ -71,10 +53,14 @@ void summon() {
   auto d = summon_demon(); // OK
   auto g = summon_ghost(); // OK
 
-  summon_demon(); // Compiler Warning
-  summon_ghost(); // Compiler Warning
+  // Compiler Warning, because returned demon is not kept
+  summon_demon();
+
+  // Compiler Warning, because the function of summoning is nodiscard
+  summon_ghost();
 }
 
+// ommit compiler warning if thing1 on thing2 is not used
 [[maybe_unused]] void f([[maybe_unused]] bool thing1,
                         [[maybe_unused]] bool thing2) {
   [[maybe_unused]] bool b = thing1 && thing2;
@@ -83,6 +69,31 @@ void summon() {
              // no warning because it is declared [[maybe_unused]]
   assert(thing1 &&
          thing2); // parameters thing1 and thing2 are not used, no warning
+}
+
+//
+// Example for carrying memory dependence
+//
+void opaque_func(int *p){/* do something with p */};
+
+[[carries_dependency]] void transparent_func(int *p) {
+  /* do something with p */
+}
+
+void illustrate_carries_dependency() {
+  std::atomic<int *> p;
+  int *atomic = p.load(std::memory_order_consume);
+
+  if (atomic)
+    std::cout << *atomic << std::endl; // transparent for the the compiler
+
+  if (atomic)
+    opaque_func(atomic); // if from another compile unit and not inline the
+                         // compiler might construct a memory fence here
+
+  if (atomic)
+    transparent_func(atomic); // marked as to work in the same memory-dependency
+                              // tree, compiler can omit the memory fence
 }
 
 int main(int argc, char **argv) {}
