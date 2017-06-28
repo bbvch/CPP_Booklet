b66c197d0eaebac94861889dec5d30735ff3d93e
Dominik Berner <dominik.berner@@bbv.ch>
2017-06-28 14:49:29 +0200

    reformated and translated lambda-example

--- lambdas.cc
+++ lambdas.cc
@@ -3,45 +3,44 @@
 * of the C++ Booklet (https://goo.gl/VJ4T3A)
 * published by bvv software services AG (c) 2017
 *
+* This example illustrates the usage of lambda functions
 **/
 
 #include <algorithm>
 #include <vector>
 
 int main(int, char **) {
+  // declaration of a lambda function
   const auto square = [](int x) { return x * x; };
+  // execution of the lambda above
   const auto r = square(4);
+
+  // the parameter brackes () can be omitted if  empty set
+  const auto abbreviated = [] { return 55; };
+
+  // using auto a parameter types for even more generic programming
+  const auto auto_param_function = [](const auto &a, const auto &b) {
+    return a * b;
+  };
 
   int x, y, z;
 
-  const auto no_capture = []() {};             // keine Zugriffsdeklaration
-  const auto capture_by_value = [x, y]() {};   // Variablen by value
-  const auto capture_by_ref = [&x, &y]() {};   // Variablen by reference
-  const auto mixed_capture = [x, &y, &z]() {}; // gemischter Zugriff
-  const auto capture_all_by_value = [=]() {
-  }; // captures all variables in scope by value (square, r, x,y,z)
-  const auto capture_all_by_ref = [&]() {}; // alle Variablen by reference
-  const auto capture_all_by_value_except = [=, &x]() {
-  }; // alle by value ausser x by ref
-  const auto captire_all_by_ref_except = [&, x]() {
-  }; // Capture all by reference except x
+  const auto no_capture = []() {};           // No variables are acptured
+  const auto capture_by_value = [x, y]() {}; // x and y are captured by value
+  const auto capture_by_ref = [&x, &y]() {
+  }; // x and y are captured by reference
+  const auto mixed_capture = [x, &y, &z]() {}; // mixed captures
 
-  // using lambda with stransform
-  std::vector<int> a1{1, 2, 3, 4, 5};
-  std::vector<int> a2(a1.size());
-  // Fülle einen Vektor mit den Quadraten eines anderen
-  // Vektors
-  std::transform(a1.begin(), a1.end(), a2.begin(), square);
+  // captures all variables in scope by value (square, r, x,y,z)
+  const auto capture_all_by_value = [=]() {};
+  // all variables in scope by reference
+  const auto capture_all_by_ref = [&]() {};
 
-  std::vector<int> v{1, 2, 3, 4, 5};
-  int sum{0};
-  // Summe der Elemente von v
-  std::for_each(v.begin(), v.end(), [&sum](int i) { sum += i; });
+  // all variables in scope by value, except x which is captured by reference
+  const auto capture_all_by_value_except = [=, &x]() {};
 
-  std::transform(a1.begin(), a1.end(), a2.begin(), [](int x) -> int {
-    int y = x % 5;
-    return x * y;
-  });
+  // capture all by reference except x
+  const auto capture_all_by_ref_except = [&, x]() {};
 
   // initialized lambda captures
   auto init_function = [&r = x, x = x + 1 ]()->int {
@@ -49,7 +48,18 @@ int main(int, char **) {
     return x + 2;
   };
 
-  const auto auto_param_function = [](const auto &a, const auto &b) {
-    return a * b;
-  };
+  // using lambda with std::-functions
+  std::vector<int> a1{1, 2, 3, 4, 5};
+  std::vector<int> a2(a1.size());
+  // fills a vector with the squares of the valus of another vectoir
+  std::transform(a1.begin(), a1.end(), a2.begin(), square);
+
+  // same but with explicit trailing return ty[e
+  std::transform(a1.begin(), a1.end(), a2.begin(),
+                 [](int x) -> int { return x * x; });
+
+  std::vector<int> v{1, 2, 3, 4, 5};
+  int sum{0};
+  // sums up the values of vector v
+  std::for_each(v.begin(), v.end(), [&sum](int i) { sum += i; });
 }
