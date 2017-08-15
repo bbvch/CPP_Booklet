0279560d75ea1cb28aec91258e7c7ce36f2d5216
Dominik Berner <dominik.berner@bbv.ch>
2017-08-15 09:06:32 +0200

    generated html output

--- generate_html.sh
+++ generate_html.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#this will open vim for each file and save it as html in the source/<filenamw>.cc.html 
 for i in source/*.cc; do vim -c TOhtml -c wqa $i; done
+
+#move all the generated files to the /html folder
+mv source/*.html html/
