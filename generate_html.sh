80f725f881ef7c0434c029e6872bf7135fdf0806
Dominik Berner <dominik.berner@bbv.ch>
2017-08-15 10:28:24 +0200

    A more printer friendly colorscheme was used

--- generate_html.sh
+++ generate_html.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-BBV_BACKGROUND=Gray
+BBV_BACKGROUND=LightGray
 NORMAL=Black
-COMMENT=Green
-CONSTANT=Red
-PREPROC=Magenta
-TYPE=Blue
-STATEMENT=Blue
+COMMENT=DarkGreen
+CONSTANT=DarkRed
+PREPROC=DarkMagenta
+TYPE=DarkBlue
+STATEMENT=DarkBlue
 
 #this will open vim for each file and save it as html in the source/<filenamw>.cc.html 
-for i in source/*.cc; do vim -c "highlight Normal ctermbg=${BBV_BACKGROUND} ctermfg=${NORMAL}" \
+for i in source/*.cc; do vim -c "set t_Co=256"\
+    -c "highlight Normal ctermbg=${BBV_BACKGROUND} ctermfg=${NORMAL}" \
 	-c "highlight Comment ctermbg=${BBV_BACKGROUND} ctermfg=${COMMENT}"\
 	-c "highlight Constant ctermbg=${BBV_BACKGROUND} ctermfg=${CONSTANT}"\
 	-c "highlight Statement ctermbg=${BBV_BACKGROUND} ctermfg=${STATEMENT}"\
