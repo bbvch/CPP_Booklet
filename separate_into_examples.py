f4d9466c3e71e347a07512d92fe1c2c5b105b0f3
Dominik Berner <doeme@weltuntergang.ch>
2017-09-05 16:06:34 +0200

    added generated folder to gitignore

---
+++ separate_into_examples.py
@@ -0,0 +1,37 @@
+import glob
+import os
+
+if not os.path.exists("./examples"):
+	os.makedirs("./examples")
+
+for f in glob.glob("source/*.cc"):
+	f_in = open(f)
+	
+	lines = f_in.readlines()
+	examples = {}
+	current_example = None
+	for l in lines:
+		
+		if l.startswith("//@"):
+			prefix, num = l.strip().split('@')
+			if current_example is not None and num != current_example:
+				print "Error: Previous example did not end"
+			elif current_example is None:
+				current_example = num
+				examples[current_example] = []
+				print "Starting example " + str(current_example)
+			elif current_example == num:  #end
+				current_example = None
+			else:
+				print "I forgot to implement something"
+		elif current_example is not None:
+			if not l.startswith("#ifdef") and not l.startswith("#endif"): #filter out non-compilation warnings
+				examples[current_example].append(l)
+	
+	f_in.close()
+	if len(examples) > 0:
+		for k in examples:
+			bname = os.path.basename(f)
+			f_out = open("examples/example_" + str(k) + "_chapter_" + bname, 'w+')
+			f_out.writelines(examples[k])
+			f_out.close()
