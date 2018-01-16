38bc664e2ccada3c4981800efd6c26e1823257d6
Dominik Berner <dominik.berner@bbv.ch>
2018-01-16 10:18:43 +0100

    Added example how to make a class bindable

--- source/2_19_structured_bindings.cc
+++ source/2_19_structured_bindings.cc
@@ -12,6 +12,7 @@
 #include <array>
 #include <map>
 #include <tuple>
+#include <vector>
 //@39
 struct Packed {
   int x;
@@ -24,7 +25,40 @@ public:
   int m;
   float n;
 };
+
 //@39
+
+// this illustrates how to make a class support structured bindings
+class Bindable
+{
+  public:
+    template<std::size_t N>
+    decltype(auto) get() const {
+
+      // note the changing of the type from std::string to const char* for the 2nd parameter
+      // of returning a reference to the vector
+      if constexpr (N == 0) return x;
+      else if constexpr (N == 1) return msg.c_str(); 
+      else if constexpr (N == 2) return (v);
+
+    }
+  private:
+    int x{123};
+    std::string msg{"ABC"};
+    std::vector<int> v{1,2,3};
+};
+/// template specialisation for class Bindable
+namespace std{
+  template<>
+  struct tuple_size<Bindable> : std::integral_constant<std::size_t, 3> {};
+
+  template<std::size_t N>
+  struct tuple_element<N, Bindable> {
+    using type = decltype(std::declval<Bindable>().get<N>());
+  };
+
+}
+
 int main() {
 //@39
   auto tuple = std::make_tuple(1, 'a', 2.3);
@@ -50,5 +84,8 @@ int main() {
     // do something
   }
 //@39
+
+  Bindable b;
+  const auto [ii, jj, kk] = b;
   return 0;
 }
