c76574060d4682b43b76e24c3a9a725b4ef22a6b
Dominik Berner <dominik.berner@gmail.com>
2018-02-08 15:23:18 +0100

    Extended example for using declarations

--- source/using_declarations.cc
+++ source/using_declarations.cc
@@ -1,4 +1,13 @@
-
+/**
+* This example does not have corresponding section in the booklet
+* of the C++ Booklet (https://goo.gl/VJ4T3A)
+* published by bbv software services AG (c) 2017
+*
+* This example shows how names declared somehwere else can be used in a declarative region
+*
+* SPDX-License-Identifier: GPL-3.0
+*
+**/
 
 void f(){
     // Do something
@@ -7,12 +16,15 @@ void f(){
 namespace X
 {
     void x() {};
+    void y() {};
+    void z() {};
 }
 
 namespace A::B::C
 {
     using ::f; // f() is available in A::B::C now
     using X::x; // x() is available in A::B::C (dropped namespace X) 
+    using X::y, X::z;
 }
 
 int main(int, char**)
