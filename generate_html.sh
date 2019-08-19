e98e5248f2cdabc3b587ec5f4d8874fc4952c53a
Dominik Berner <dominik.berner@gmail.com>
2019-08-19 15:12:53 +0200

    Removed cmake-based html generation

--- generate_html.sh
+++ generate_html.sh
@@ -10,15 +10,16 @@ STATEMENT=DarkBlue
 
 SOURCEDIR=examples
 
-#this will open vim for each file and save it as html in the src/<filenamw>.cc.html 
+#this will open vim for each file and save it as html in the src/<filenamw>.cc.html
 for i in ${SOURCEDIR}/*.cc; do vim -c "set t_Co=256"\
     -c "highlight Normal ctermbg=${BBV_BACKGROUND} ctermfg=${NORMAL}" \
-	-c "highlight Comment ctermbg=${BBV_BACKGROUND} ctermfg=${COMMENT}"\
-	-c "highlight Constant ctermbg=${BBV_BACKGROUND} ctermfg=${CONSTANT}"\
-	-c "highlight Statement ctermbg=${BBV_BACKGROUND} ctermfg=${STATEMENT}"\
-	-c "highlight Type ctermbg=${BBV_BACKGROUND} ctermfg=${TYPE}"\
-	-c "highlight PreProc ctermbg=${BBV_BACKGROUND} ctermfg=${PREPROC}"\
-	-c TOhtml -c wqa $i; done
+    -c "highlight Comment ctermbg=${BBV_BACKGROUND} ctermfg=${COMMENT}"\
+    -c "highlight Constant ctermbg=${BBV_BACKGROUND} ctermfg=${CONSTANT}"\
+    -c "highlight Statement ctermbg=${BBV_BACKGROUND} ctermfg=${STATEMENT}"\
+    -c "highlight Type ctermbg=${BBV_BACKGROUND} ctermfg=${TYPE}"\
+    -c "highlight PreProc ctermbg=${BBV_BACKGROUND} ctermfg=${PREPROC}"\
+-c TOhtml -c wqa $i; done
 
 #move all the generated files to the /html folder
 mv ${SOURCEDIR}/*.html html/
+
